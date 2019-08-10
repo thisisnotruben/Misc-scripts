e10e187d6776986db54f4b0461f7c369a3a4f411
thisisnotruben <rubenar1996@gmail.com>
2019-08-10 12:47:27 -0700

    Downloads programs on a new Linux install

---
+++ DownloadPrograms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update;
+sudo apt-get upgrade -y;
+
+sudo apt-get install gimp vlc chromium-browser clementine bleachbit deluge audacity libreoffice calibre -y
+
+sudo apt-get update;
+sudo apt-get upgrade -y;
