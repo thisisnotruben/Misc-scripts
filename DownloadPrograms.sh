62762a8353c2943d03e6bb7bf1a4cde9fe33ed0c
thisisnotruben <rubenar1996@gmail.com>
2019-08-14 08:14:27 -0700

    Update DownloadPrograms.sh

--- DownloadPrograms.sh
+++ DownloadPrograms.sh
@@ -3,7 +3,7 @@
 sudo apt-get update;
 sudo apt-get upgrade -y;
 
-sudo apt-get install gimp vlc chromium-browser clementine bleachbit deluge audacity libreoffice calibre -y
+sudo apt-get install gimp vlc chromium-browser clementine bleachbit deluge audacity libreoffice calibre git youtube-dl blender zim -y
 
 sudo apt-get update;
 sudo apt-get upgrade -y;
